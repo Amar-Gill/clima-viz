3b588c9eaaebeb5409b898ed8a00629bc8fa6b67
Amar Gill <amardeep@clearbanc.com>
2022-02-12 10:42:53 -0500

    added click handler

--- components/Map.tsx
+++ components/Map.tsx
@@ -1,4 +1,31 @@
-import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
+import { LatLngExpression } from 'leaflet'
+import { useState } from 'react'
+import {
+  MapContainer,
+  TileLayer,
+  Marker,
+  Popup,
+  useMapEvents,
+} from 'react-leaflet'
+
+const LocationMarker = () => {
+  const [position, setPosition] = useState<LatLngExpression | null>(null)
+  const map = useMapEvents({
+    click(e) {
+      setPosition(e.latlng)
+      map.flyTo(e.latlng, map.getZoom())
+    },
+  })
+
+  return position === null ? null : (
+    <Marker position={position}>
+      <Popup>
+        Project Coordinates: <br />
+        {position.toString()}
+      </Popup>
+    </Marker>
+  )
+}
 
 const Map = () => {
   return (
@@ -7,11 +34,7 @@ const Map = () => {
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
-      <Marker position={[51.505, -0.09]}>
-        <Popup>
-          A pretty CSS3 popup. <br /> Easily customizable.
-        </Popup>
-      </Marker>
+      <LocationMarker />
     </MapContainer>
   )
 }
