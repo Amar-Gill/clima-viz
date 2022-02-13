10276a1713305babfe30fe0b56f6e8be0bb206c8
Amar Gill <amardeep@clearbanc.com>
2022-02-13 16:01:43 -0500

    added linechart

--- pages/solar-calculations/index.tsx
+++ pages/solar-calculations/index.tsx
@@ -1,5 +1,27 @@
 import type { LatLngTuple } from 'leaflet'
+import { Line } from 'react-chartjs-2'
 import SolarCalculator from '../../utils/solar-calculator'
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend,
+} from 'chart.js'
+import { addDays } from 'date-fns'
+
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend
+)
 
 const testData = [51.51, -0.06] as LatLngTuple
 
@@ -7,16 +29,43 @@ const calculator = new SolarCalculator(testData)
 
 const now = new Date()
 
-const equationOfTime = calculator.calculateEquationOfTime(now)
+const currentYear = now.getUTCFullYear()
 
-const solarDeclination = calculator.calculateSolarDeclination(now)
+const startDate = new Date(`January 1, ${currentYear}`)
+
+// to do handle leapyears
+const arrayYear = new Array(365).fill(null)
+
+const options = {
+  responsive: true,
+  plugins: {
+    legend: {
+      position: 'top' as const,
+    },
+    title: {
+      display: true,
+      text: `Solar Declination Angle - ${currentYear}`,
+    },
+  },
+}
+
+const data = {
+  labels: arrayYear.map((v, i) => i),
+  datasets: [
+    {
+      label: 'Solar Declination Angle',
+      data: arrayYear.map((v, i) =>
+        calculator.calculateSolarDeclination(addDays(startDate, i))
+      ),
+    },
+  ],
+}
 
 const SolarCalculations = () => {
   return (
     <div>
       <p>data: {testData}</p>
-      <p>equationOfTime: {equationOfTime}</p>
-      <p>solarDeclination: {solarDeclination}</p>
+      <Line options={options} data={data} />
     </div>
   )
 }
