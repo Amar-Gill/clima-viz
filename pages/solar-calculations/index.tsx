db1a9ea07b4fbc06321df8147a1e36e441664fa9
Amar Gill <amardeep@clearbanc.com>
2022-02-15 23:38:39 -0500

    can set year for solar charts

--- pages/solar-calculations/index.tsx
+++ pages/solar-calculations/index.tsx
@@ -13,6 +13,7 @@ import useStore from '../../utils/store'
 import SolarDeclinationChart from '../../components/SolarDeclinationChart'
 import EquationOfTimeChart from '../../components/EquationOfTimeChart'
 import { startOfYear } from 'date-fns'
+import { useState } from 'react'
 
 ChartJS.register(
   CategoryScale,
@@ -24,16 +25,31 @@ ChartJS.register(
   Legend
 )
 
-const startDate = startOfYear(new Date())
+const now = new Date()
+const startYear = now.getUTCFullYear()
+const options = new Array(11).fill(null).map((_, i) => -5 + i + startYear)
 
 const SolarCalculations = () => {
   const { position } = useStore((state) => state)
+  const [startDate, setStartDate] = useState(startOfYear(now))
 
   const calculator = position ? new SolarCalculator(position) : null
 
   return (
     <div>
-      <p>data: {position?.toString()}</p>
+      <p>Position: {position?.toString()}</p>
+      <label htmlFor="select-year">Select Year: </label>
+      <select
+        id="select-year"
+        defaultValue={startYear}
+        onChange={(e) => setStartDate(new Date(`${e.target.value}-01-01`))}
+      >
+        {options.map((v) => (
+          <option value={v} key={v}>
+            {v}
+          </option>
+        ))}
+      </select>
       {calculator && (
         <>
           <SolarDeclinationChart
