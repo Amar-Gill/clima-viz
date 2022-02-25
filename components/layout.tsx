d1fb9ba79834299ae860d5f8c1a6db1b82bd523d
Amar Gill <amardeep@clearbanc.com>
2022-02-24 21:16:05 -0500

    created NavLink component

--- components/layout.tsx
+++ components/layout.tsx
@@ -1,22 +1,40 @@
-import Link from 'next/link';
+import Link, { LinkProps } from 'next/link';
+import { useRouter } from 'next/router';
+import React from 'react';
+
+const NavLink = ({ href, children }: React.PropsWithChildren<LinkProps>) => {
+  const router = useRouter();
+
+  const dynamicClass =
+    router.pathname === href
+      ? 'bg-zinc-200 font-bold border-y border-zinc-400'
+      : 'hover:bg-zinc-200';
+  return (
+    <Link href={href} passHref>
+      <a>
+        <div className={`px-2 py-1 ${dynamicClass}`}>{children}</div>
+      </a>
+    </Link>
+  );
+};
 
 const Layout: React.FC<{}> = ({ children }) => {
   return (
     <>
-      <nav className="fixed h-screen w-40 bg-slate-200">
+      <nav className="fixed h-screen w-40 border-r border-zinc-400 bg-zinc-50 shadow shadow-zinc-400">
         <ul>
           <li>
-            <Link href="/">Home</Link>
+            <NavLink href="/">Home</NavLink>
           </li>
           <li>
-            <Link href="/site-location">Site Location</Link>
+            <NavLink href="/site-location">Site Location</NavLink>
           </li>
           <li>
-            <Link href="/solar-calculations">Solar Calculations</Link>
+            <NavLink href="/solar-calculations">Solar Calculations</NavLink>
           </li>
         </ul>
       </nav>
-      <main className="container mx-auto pl-40">{children}</main>
+      <main className="container mx-auto bg-zinc-50 pl-40">{children}</main>
     </>
   );
 };
