3b8a95b810f84df85748fe5cb09748c7641be8ec
Amar Gill <amardeep@clearbanc.com>
2022-03-01 19:47:12 -0500

    made layout mobile responsive with custom mobile navigation

--- components/layout.tsx
+++ components/layout.tsx
@@ -1,6 +1,6 @@
 import Link, { LinkProps } from 'next/link';
 import { useRouter } from 'next/router';
-import React from 'react';
+import React, { useState } from 'react';
 
 const NavLink = ({ href, children }: React.PropsWithChildren<LinkProps>) => {
   const router = useRouter();
@@ -18,10 +18,26 @@ const NavLink = ({ href, children }: React.PropsWithChildren<LinkProps>) => {
   );
 };
 
+const NavLinkMobile = ({ href, children }: React.PropsWithChildren<LinkProps>) => {
+  const router = useRouter();
+
+  const dynamicClass = router.pathname === href ? 'bg-zinc-300' : '';
+  return (
+    <Link href={href} passHref>
+      <a>
+        <span className={'rounded-lg p-2 text-3xl font-bold' + ' ' + dynamicClass}>
+          {children}
+        </span>
+      </a>
+    </Link>
+  );
+};
+
 const Layout: React.FC<{}> = ({ children }) => {
+  const [showMobileMenu, setShowMobileMenu] = useState(false);
   return (
     <div className="bg-zinc-50 text-zinc-900">
-      <nav className="fixed h-screen w-40 border-r border-zinc-400 shadow shadow-zinc-400">
+      <nav className="fixed hidden h-screen w-40 border-r border-zinc-400 shadow shadow-zinc-400 sm:block">
         <ul>
           <li>
             <NavLink href="/">Home</NavLink>
@@ -34,7 +50,58 @@ const Layout: React.FC<{}> = ({ children }) => {
           </li>
         </ul>
       </nav>
-      <main className="container mx-auto min-h-screen pl-40">{children}</main>
+      <nav className="min-h-6 flex w-screen justify-between sm:hidden">
+        <span>Ifrit</span>
+        <span onClick={() => setShowMobileMenu(true)}>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            strokeWidth="2"
+            strokeLinecap="round"
+            strokeLinejoin="round">
+            <line x1="3" y1="12" x2="21" y2="12" />
+            <line x1="3" y1="6" x2="21" y2="6" />
+            <line x1="3" y1="18" x2="21" y2="18" />
+          </svg>
+        </span>
+      </nav>
+      {showMobileMenu && (
+        <div className="fixed top-0 h-screen w-screen bg-zinc-50">
+          <div className="flex justify-end" onClick={() => setShowMobileMenu(false)}>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="32"
+              height="32"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              strokeWidth="2"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              className="feather feather-x-square">
+              <rect x="3" y="3" width="18" height="18" rx="2" ry="2" />
+              <line x1="9" y1="9" x2="15" y2="15" />
+              <line x1="15" y1="9" x2="9" y2="15" />
+            </svg>
+          </div>
+          <ul className="pl-4" onClick={() => setShowMobileMenu(false)}>
+            <li className="my-2">
+              <NavLinkMobile href="/">Home</NavLinkMobile>
+            </li>
+            <li className="my-2">
+              <NavLinkMobile href="/site-location">Site Location</NavLinkMobile>
+            </li>
+            <li className="my-2">
+              <NavLinkMobile href="/solar-calculations">Solar Calculations</NavLinkMobile>
+            </li>
+          </ul>
+        </div>
+      )}
+      <main className="container mx-auto min-h-screen sm:pl-40">{children}</main>
     </div>
   );
 };
