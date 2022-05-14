05dc08986694836a604322880225bd3d0365fd88
Amar Gill <amardeep@clearbanc.com>
2022-05-14 00:37:50 -0400

    memoize control construction

--- components/AddressSearch.tsx
+++ components/AddressSearch.tsx
@@ -1,62 +1,66 @@
 import { GeocoderAutocomplete } from '@geoapify/geocoder-autocomplete';
 import { Control, DomUtil, LatLng, Map } from 'leaflet';
 import useStore from 'lib/store';
-import { useEffect } from 'react';
+import { useEffect, useMemo } from 'react';
 import { useMap } from 'react-leaflet';
 
 export const AddressSearch = () => {
   const mapContainer = useMap();
   const { setPosition } = useStore((state) => state);
 
-  const SearchControl = Control.extend({
-    options: {
-      position: 'topright',
-    },
-    onAdd: function (map: Map) {
-      const el = DomUtil.create('div');
+  const searchControl = useMemo(() => {
+    const SearchControl = Control.extend({
+      options: {
+        position: 'topright',
+      },
+      onAdd: function (map: Map) {
+        const el = DomUtil.create('div');
 
-      el.className = 'relative minimal round-borders';
+        el.className = 'relative minimal round-borders';
 
-      el.addEventListener('click', (e) => {
-        e.stopPropagation();
-      });
+        el.addEventListener('click', (e) => {
+          e.stopPropagation();
+        });
 
-      el.addEventListener('dblclick', (e) => {
-        e.stopPropagation();
-      });
+        el.addEventListener('dblclick', (e) => {
+          e.stopPropagation();
+        });
 
-      const autocomplete = new GeocoderAutocomplete(
-        el,
-        `${process.env.NEXT_PUBLIC_GEOAPIFY}`,
-        {
-          placeholder: 'Enter an address',
-        },
-      );
+        const autocomplete = new GeocoderAutocomplete(
+          el,
+          `${process.env.NEXT_PUBLIC_GEOAPIFY}`,
+          {
+            placeholder: 'Enter an address',
+          },
+        );
 
-      autocomplete.on('select', (location) => {
-        if (!location) {
+        autocomplete.on('select', (location) => {
+          if (!location) {
+            return;
+          }
+          const { lat, lon } = location.properties;
+          const newPosition = new LatLng(lat, lon);
+          setPosition(newPosition);
+          map.setView(newPosition, map.getZoom());
+        });
+
+        autocomplete.on('suggestions', (suggestions) => {
           return;
-        }
-        const { lat, lon } = location.properties;
-        const newPosition = new LatLng(lat, lon);
-        setPosition(newPosition);
-        map.setView(newPosition, map.getZoom());
-      });
+        });
 
-      autocomplete.on('suggestions', (suggestions) => {
+        return el;
+      },
+      onRemove: function (map: Map) {
         return;
-      });
+      },
+    });
 
-      return el;
-    },
-    onRemove: function (map: Map) {
-      return;
-    },
-  });
+    return new SearchControl();
+  }, [setPosition]);
 
   useEffect(() => {
-    mapContainer.addControl(new SearchControl());
-  }, []);
+    mapContainer.addControl(searchControl);
+  }, [mapContainer, searchControl]);
 
   return null;
 };
