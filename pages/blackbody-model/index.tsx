46c3f06e77a3fa7ae1f3ed83e5448b1f399dd97c
Amar Gill <amardeep@clearbanc.com>
2022-07-05 22:33:11 -0400

    can render basic box

--- pages/blackbody-model/index.tsx
+++ pages/blackbody-model/index.tsx
@@ -1,5 +1,21 @@
+import { OrbitControls } from '@react-three/drei';
+import { Canvas } from '@react-three/fiber';
+
 const BlackBodyModel = () => {
-  return <div>todo</div>;
+  return (
+    <div className="h-screen w-full">
+      <Canvas>
+        <OrbitControls />
+        <gridHelper />
+        <ambientLight />
+        <pointLight intensity={4} position={[10, 8, 6]} />
+        <mesh>
+          <boxGeometry args={[1, 1, 1]} />
+          <meshStandardMaterial color="#24242e" />
+        </mesh>
+      </Canvas>
+    </div>
+  );
 };
 
 export default BlackBodyModel;
