0474b8e4e5436e5b19d7643a674de0431d339895
Amar Gill <amar.s.gill@outlook.com>
2022-07-08 23:57:37 -0400

    added leva package. use basic controls for box geometry

--- pages/blackbody-model/index.tsx
+++ pages/blackbody-model/index.tsx
@@ -1,7 +1,29 @@
 import { OrbitControls } from '@react-three/drei';
 import { Canvas } from '@react-three/fiber';
+import { useControls } from 'leva';
 
 const BlackBodyModel = () => {
+  const { width, height, depth } = useControls({
+    width: {
+      value: 1,
+      min: 1,
+      max: 10,
+      step: 1,
+    },
+    height: {
+      value: 1,
+      min: 1,
+      max: 10,
+      step: 1,
+    },
+    depth: {
+      value: 1,
+      min: 1,
+      max: 10,
+      step: 1,
+    },
+  });
+
   return (
     <div className="h-screen w-full">
       <Canvas>
@@ -9,8 +31,8 @@ const BlackBodyModel = () => {
         <gridHelper />
         <ambientLight />
         <pointLight intensity={4} position={[10, 8, 6]} />
-        <mesh>
-          <boxGeometry args={[1, 1, 1]} />
+        <mesh position-y={0.5}>
+          <boxGeometry args={[width, height, depth]} />
           <meshStandardMaterial color="#24242e" />
         </mesh>
       </Canvas>
