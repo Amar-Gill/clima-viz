263acdd1def3144162e8646f4275ed41a3267ab6
Amar Gill <amardeep@clearbanc.com>
2022-07-13 20:30:59 -0400

    update utils

--- lib/utils.ts
+++ lib/utils.ts
@@ -38,3 +38,46 @@ export function getChartLabels() {
     labelsLeapYear,
   };
 }
+
+/**
+ *
+ * @param {number} numDays the number of days as a decimal value
+ * @returns {string} string representing the time of day in the format 'hh:mm:ss'
+ */
+export function convertDaysToTimeString(numDays: number): string {
+  // account for roll over time where dayFraction > 1
+  const singleDayFraction = numDays % 1;
+
+  const hours = singleDayFraction * 24;
+  const truncatedHours = Math.trunc(hours);
+
+  const minutes = (hours - truncatedHours) * 60;
+  const truncatedMinutes = Math.trunc(minutes);
+
+  const seconds = (minutes - truncatedMinutes) * 60;
+  const truncatedSeconds = Math.trunc(seconds);
+
+  const hh =
+    truncatedHours.toString().length === 1 ? `0${truncatedHours}` : truncatedHours;
+  const mm =
+    truncatedMinutes.toString().length === 1 ? `0${truncatedMinutes}` : truncatedMinutes;
+  const ss =
+    truncatedSeconds.toString().length === 1 ? `0${truncatedSeconds}` : truncatedSeconds;
+
+  return `${hh}:${mm}:${ss}`;
+}
+
+/**
+ *
+ * @param date
+ * @param tzString
+ * @returns {Date}
+ * {@link https://stackoverflow.com/questions/10087819/convert-date-to-another-timezone-in-javascript}
+ */
+export function convertTZ(date: Date, tzString: string) {
+  return new Date(
+    (typeof date === 'string' ? new Date(date) : date).toLocaleString('en-US', {
+      timeZone: tzString,
+    }),
+  );
+}
