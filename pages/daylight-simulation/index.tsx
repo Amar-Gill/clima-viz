8fcc660f73657548573bad8d9fd7db1ebea0506b
Amar Gill <amardeep@clearbanc.com>
2022-10-15 13:52:20 -0400

    update daylight-sim page to use new api

--- pages/daylight-simulation/index.tsx
+++ pages/daylight-simulation/index.tsx
@@ -1,5 +1,6 @@
 import { Line, OrbitControls } from '@react-three/drei';
 import { Canvas } from '@react-three/fiber';
+import { getDayOfYear } from 'date-fns';
 import SolarCalculator from 'lib/solar-calculator';
 import useStore from 'lib/store';
 import { convertDaysToTimeString } from 'lib/utils';
@@ -10,13 +11,7 @@ import { radToDeg } from 'three/src/math/MathUtils';
 
 const DaylightSimulation = () => {
   /** Initialize local state */
-  const now = new Date();
-  let m = 720;
-  let d = new Date(now.getUTCFullYear(), 0, 1, 0, m, 0, 0);
-  d = new Date(d.toLocaleString('en-US', { timeZone: 'Greenwich' }));
-
-  const [minutes, setMinutes] = useState(m);
-  const [date, setDate] = useState(d);
+  const [minutes, setMinutes] = useState(0);
   const { position } = useStore((state) => state);
 
   const calculator = position ? new SolarCalculator(position) : undefined;
@@ -24,14 +19,15 @@ const DaylightSimulation = () => {
   const handleMinutesChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const m = Number(e.target.value);
     setMinutes(m);
-    const newDate = new Date(2022, 0, 1, 0, m, 0, 0);
-    setDate(new Date(newDate.toLocaleString('en-US', { timeZone: 'Greenwich' })));
   };
+
+  // hard code dayOfYear to be present day for now
+  const dayOfYear = getDayOfYear(new Date());
 
   // https://mathinsight.org/spherical_coordinates
   const radius = 5;
-  const zenith = calculator?.zenithAngle(date) ?? 0;
-  const azimuth = calculator?.azimuthAngle(date) ?? 0;
+  const zenith = calculator?.zenithAngle(dayOfYear, minutes) ?? 0;
+  const azimuth = calculator?.azimuthAngle(dayOfYear, minutes) ?? 0;
 
   const x = radius * Math.sin(zenith) * Math.cos(azimuth);
   const y = radius * Math.sin(zenith) * Math.sin(azimuth);
@@ -53,15 +49,21 @@ const DaylightSimulation = () => {
               min="0"
               max="1440"
               step="10"
-              name="hours"
+              name="minutes"
               value={minutes}
               onChange={handleMinutesChange}
             />
             <label htmlFor="hours">time: {convertDaysToTimeString(minutes / 1440)}</label>
           </div>
           <div>
-            <div>elevation: {radToDeg(calculator.elevationAngle(date))}</div>
+            <div>
+              elevation: {radToDeg(calculator.elevationAngle(dayOfYear, minutes))}
+            </div>
             <div>azimuth: {radToDeg(azimuth)}</div>
+            <div>utc offset: {calculator.calculateUTCOffset()}</div>
+            <div>x: {x}</div>
+            <div>y: {y}</div>
+            <div>z: {z}</div>
           </div>
           <div className="h-screen w-full">
             <Canvas camera={{ position: [3, 3, 3] }}>
